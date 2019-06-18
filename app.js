8536bce8b134632ce428e69f2ecf02609653e126
Braun Okoi Boniface <ldbraun@live.com>
2019-06-18 12:49:48 +0100

    Changed required to point to class

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const BankAccount = require('./BankAccount');
+const BankAccount = require('./BankAccount.js');
 
 var app = express();
 
